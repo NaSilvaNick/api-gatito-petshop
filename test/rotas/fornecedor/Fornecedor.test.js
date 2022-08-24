21d3fb2a11e7e8bd9c72d2d611e9f99ad9cc88ad
Nickolas Silva <77502906+NaSilvaNick@users.noreply.github.com>
2022-08-23 21:52:16 -0300

    Teste do metodo criar adicionado

--- test/rotas/fornecedor/Fornecedor.test.js
+++ test/rotas/fornecedor/Fornecedor.test.js
@@ -1,14 +1,30 @@
-const Fornecedor = require("../../../api/rotas/fornecedores/Fornecedor");
+jest.mock('../../../api/rotas/fornecedores/TabelaFornecedor')
+const Fornecedor = require('../../../api/rotas/fornecedores/Fornecedor')
 
-describe("classe Fornecedor", () => {
-  test("O método validar() retorna true", () => {
+describe('Classe Fornecedor', () => {
+  test('O método validar() retorna true', () => {
     const fornecedor = new Fornecedor({
-      empresa: "Gatito",
-      email: "contato@gatito.com.br",
-      categoria: "brinquedos"
-    });
-    fornecedor.validar();
+      empresa: 'Gatito',
+      email: 'contato@gatito.com.br',
+      categoria: 'brinquedos'
+    })
+    fornecedor.validar()
 
-    expect(fornecedor.validar()).toBe(true);
-  });
-});
+    expect(fornecedor.validar()).toBe(true)
+  })
+
+  test('O método criar() foi executado com sucesso', async () => {
+    const fornecedor = new Fornecedor({
+      empresa: 'Gatito',
+      email: 'contato@gatito.com.br',
+      categoria: 'brinquedos'
+    })
+
+    await fornecedor.criar()
+
+    expect(fornecedor.id).toBe(500)
+    expect(fornecedor.data_criacao).toBe("22/08/2022")
+    expect(fornecedor.data_atualizacao).toBe("22/08/2022")
+    expect(fornecedor.versao).toBe(90)
+  })
+})
